43c9bd4676e90c4e800b2947cbf180956b3b5be4
brianpompey <brianthescholar@gmail.com>
2022-01-27 16:41:43 -0500

    using a switrh statement and ternary operator

--- src/screens/SquareSceen.js
+++ src/screens/SquareSceen.js
@@ -11,12 +11,23 @@ const SquareScreen = () => {
     const [blue, setBlue] = useState(0);
 
     const setColor = (color, change) => {
-        if (color === 'red') {
-            if (red + change > 255 || red + change < 0) {
+
+        switch (color) {
+            case 'red':
+                red + change > 255 || red + change < 0 ? null : setRed(red + change);
                 return;
-            } else {
-                setRed(red + change);
-            }
+            case 'green':
+                green + change > 255 || green + change < 0 ? null : setGreen(green + change);
+                return;
+            case 'blue':
+                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
+                return;
+                // if (red + change > 255 || red + change < 0) {
+                //     return;
+                // } else {
+                //     setRed(red + change);
+                // }
+                
         }
     };
 
