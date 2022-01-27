55e94ebedb8c345449c887e0d2294358b49bf7fb
brianpompey <brianthescholar@gmail.com>
2022-01-26 21:39:39 -0500

    using a reusable compoennt

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ImageDetail = () => {
+    return <Text>Shoiw Image of Forest</Text>
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageDetail;
