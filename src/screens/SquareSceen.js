370747666e07e74abdac47d551848e9c7b934f0b
brianpompey <brianthescholar@gmail.com>
2022-01-27 15:53:37 -0500

    Adding square screen

---
+++ src/screens/SquareSceen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+const SquareScreen = () => {
+    return(
+        <View>
+            <Text>Square Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({});
+
+export default SquareScreen;
