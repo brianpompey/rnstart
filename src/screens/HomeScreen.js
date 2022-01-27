5242b3dc0963ee3b5ed32acea330ed90f3cd40d3
brianpompey <brianthescholar@gmail.com>
2022-01-26 21:27:23 -0500

    props destructure

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button, Pressable, TouchableOpacity } from 'react-native';
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
 
 
   return (
     <View>
       <Text style={styles.text}>Hello WOrld</Text>
-      <Button 
-        onPress={() => props.navigation.navigate('Components')}
-        title="Go to Components Demo"
-      />
 
-      <Pressable onPress={() => props.navigation.navigate('List')}>
-        <Text style={styles.pressableText}>Go to new demo</Text>
+
+      <Pressable onPress={() => navigation.navigate('Components')}>
+        <Text style={styles.pressableText}>Go to Components Demo</Text>
+      </Pressable>
+
+      <Pressable onPress={() => navigation.navigate('List')}>
+        <Text style={styles.pressableText}>Go to List demo</Text>
       </Pressable>
     </View>
   );
