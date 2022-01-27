a0e8b41e09d963b2e0692f71286de010e8a2e880
brianpompey <brianthescholar@gmail.com>
2022-01-27 12:07:44 -0500

    added color demo

---
+++ src/screens/ColorScreen.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+
+
+const ColorScreen = () => {
+   //  const [counter, setCounter] = useState(0);
+
+    return (
+        <View>
+            <Button title="Add a color" />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({});
+
+export default ColorScreen;
