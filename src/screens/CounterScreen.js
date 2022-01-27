5bd4c432aa8cca22f522f762748081d2136a98fc
brianpompey <brianthescholar@gmail.com>
2022-01-27 11:04:42 -0500

    Adding state

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -3,9 +3,17 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 
 
 const CounterScreen = () => {
+    let counter = 0
+
     return (
         <View>
-            <Text>Current Count:</Text>
+            <Button title="Increase" onPress={() => {
+                counter++;
+            }}/>
+            <Button title="Decrease" onPress={() => {
+                counter--;
+            }}/>
+            <Text>Current Count: {counter} </Text>
         </View>
     )
 };
