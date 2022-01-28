dd07245b70c746a82144b05ce0f370992f8da541
brianpompey <brianthescholar@gmail.com>
2022-01-28 13:55:52 -0500

    css exercise

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,46 +3,66 @@ import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
     return(
-        <View style={styles.viewStyle} >
-            <Text style={styles.textOneStyle} >
-                Child #1
-            </Text>
-            <Text style={styles.textTwoStyle} >
-                Child #2
-            </Text>
-            <Text style={styles.textThreeStyle} >
-                Child #3
-            </Text>
+        <View style={styles.parentStyle} >
+            <View style={styles.textOneStyle} >
+         
+            </View>
+            <View style={styles.textTwoStyle} >
+           
+            </View>
+            <View style={styles.textThreeStyle} >
+           
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    viewStyle: {
+    parentStyle: {
         borderWidth: 4,
         borderColor: 'black',
         height: 200,
-        alignItems: 'center'
-       // flexDirection: 'row'
+     //   alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
     textOneStyle: {
-        borderWidth: 4,
-        borderColor: 'red',
+      //  borderWidth: 4,
+      //  borderColor: 'red',
        // flex: 4,
        // margin: 20,
+       height: 50,
+       width: 50,
+       backgroundColor: 'red',
+     //  alignSelf: 'flex-start'
     },
     textTwoStyle: {
-        borderWidth: 4,
-        borderColor: 'red',
-        alignSelf: 'stretch'
+      //  borderWidth: 4,
+      //  borderColor: 'red',
+       // position: 'absolute',
+      //  fontSize: 18,
+        // top: 0,
+        // right: 0,
+        // left: 0,
+        // bottom : 0,
+     //   ...StyleSheet.absoluteFillObject
        // flex: 2
        // margin: 20,
+       height: 50,
+       width: 50,
+       backgroundColor: 'green',
+       marginTop: 50,
+      // al
     },
     textThreeStyle: {
-        borderWidth: 4,
-        borderColor: 'red',
+      //  borderWidth: 4,
+      //  borderColor: 'red',
        // flex: 4
        // margin: 20,
+       height: 50,
+       width: 50,
+       backgroundColor: 'purple',
+     //  alignSelf: 'flex-start'
     },
 });
 
