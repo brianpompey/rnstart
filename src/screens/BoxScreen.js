cb390dc965f78a6a446f2acc111b08cc87e16e06
brianpompey <brianthescholar@gmail.com>
2022-01-28 12:39:39 -0500

    aligning items

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,14 +3,31 @@ import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
     return(
-        <View>
-            <Text>
-                BoxScreen
+        <View style={styles.viewStyle} >
+            <Text style={styles.textStyle} >
+                Child #1
+            </Text>
+            <Text style={styles.textStyle} >
+                Child #2
+            </Text>
+            <Text style={styles.textStyle} >
+                Child #3
             </Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 4,
+        borderColor: 'black',
+        alignItems: 'flex-start'
+    },
+    textStyle: {
+        borderWidth: 4,
+        borderColor: 'red',
+       // margin: 20,
+    }
+});
 
 export default BoxScreen;
